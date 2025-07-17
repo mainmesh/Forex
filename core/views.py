50385b84a5726b7526c631c4137147fd894e1dbf
mainmesh <meshackmbithi254@gmail.com>
2025-07-17 13:10:43 +0300

    Add Trade Here login-like page mimicking Deriv

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from .models import Profile
 def home(request):
     profile = Profile.objects.first()
     return render(request, 'home.html', {'profile': profile})
+
+def trade_here(request):
+    return render(request, 'trade_here.html')
--- linktree_clone/urls.py
+++ linktree_clone/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import home
+from core import views 
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('trade-here/', views.trade_here, name='trade_here'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
